dd0017c64064a26b5e7d5f9b7095082584f575b1
Ch-MANASA19 <manasachimata19@gmail.com>
2026-02-11 20:02:25 +0530

    Update 3.py

--- 3.py
+++ 3.py
@@ -1,12 +1,1 @@
-correct_username = "admin"
-correct_password = "pass123"
-account_active = True
-username = input("Enter username: ")
-password = input("Enter password: ")
-if username == correct_username and password == correct_password:
-    if account_active:
-        print("Login Successful")
-    else:
-        print("Account Disabled")
-else:
-    print("Wrong Credentials")
+
